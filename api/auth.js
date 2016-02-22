9d37f5b0f44870e86bb4d18dadd4f3755590ffd7
Benjamin Ceglowski <ben@thephuse.com>
2016-02-22 22:16:10 +0200

    Added 'use strict' statement to all JS files. Removed config.js as it threw errors instead of allowing envvars to take precedence.

--- api/auth.js
+++ api/auth.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const env = require('./env');
 
 const sessionParams = {
--- api/env.js
+++ api/env.js
@@ -1,10 +1,20 @@
-const config = require('config');
+'use strict';
+
+const fs = require('fs');
+
+let config;
+
+try {
+  config = JSON.parse(fs.readFileSync(`${process.env.PWD}/config/default.json`));
+} catch (e) {
+  config = {};
+}
 
 const env = function(variable) {
   if (process.env && process.env[variable]) {
     return process.env[variable];
-  } else if (config.get(variable)) {
-    return config.get(variable);
+  } else if (config && config[variable]) {
+    return config[variable];
   } else {
     return null;
   }
